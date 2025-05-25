cc556492361d36881dde47c3ad62c008bb28439a
Xuankiendev <Vxkiue@gmail.com>
2025-05-25 14:57:53 +0700

    Delete pages directory

--- pages/api/rps.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-  const time = new Date().toLocaleTimeString();
-  const rps = Math.floor(Math.random() * 100) + 100;
-  res.status(200).json({ time, rps });
-}
